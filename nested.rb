758f807410952d42b3b4285ac98c37e8f44e3421
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-12 10:53:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,10 +76,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto => {
+     yukihiro_matsumoto = {:yukihiro_matsumoto => {
    :known_for => "Ruby",
     :languages => ["LISP", "C"]}
- ]
+     }
+     programmer_hash.merge(yukihiro_matsumoto)
  
  return programmer_hash
     
