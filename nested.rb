b636d4f66259673cf247a933df9cb15121afbc63
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-12 10:51:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,11 +59,7 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
 
 	programmer_hash = 
  		{
@@ -80,7 +76,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
     
 end
 
@@ -100,6 +100,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
+     
+     
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
     programmer_hash[:alan_kay][:known_for] += "GUI"
      
